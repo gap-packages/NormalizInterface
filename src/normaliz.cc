13cf2efa72f2cc1bcc4f77e783f44c780833eb36
Christof Soeger <csoeger@uos.de>
2014-03-07 00:16:31 +0100

    First use of libnormaliz
    Conversion functions for vectors and matricies
    NormalizTestCone for Hilbert basis computation

--- src/normaliz.cc
+++ src/normaliz.cc
@@ -18,12 +18,113 @@
  */
 
 #include "normaliz.h"
+#include "libnormaliz/cone.h"
 
+#include <vector>
+#include <iostream>
+
+using libnormaliz::Cone;
+//using libnormaliz::ConeProperty;
+using libnormaliz::ConeProperties;
+using libnormaliz::Type::InputType;
+
+using std::map;
+using std::vector;
 
 Obj NormalizTest(Obj self, Obj A)
 {
     // we just return our parameter
     return A;
+}
+
+bool GAPIntVectorToNmz(vector<long>& out, Obj V)
+{
+    if (!IS_DENSE_PLIST(V))
+        return false;
+    const int n = LEN_PLIST(V);
+    out.resize(n);
+    for (int i=0; i < n; ++i)
+    {
+        Obj tmp = ELM_PLIST(V, i+1);
+        if (!IS_INTOBJ(tmp))
+            return false;
+        out[i] = INT_INTOBJ(tmp);
+    }
+    return true;
+}
+
+bool GAPIntMatrixToNmz(vector< vector<long> >& out, Obj M)
+{
+    if (!IS_DENSE_PLIST(M))
+        return false;
+    const int nr = LEN_PLIST(M);
+    out.resize(nr);
+    for (int i=0; i < nr; ++i)
+    {
+        bool okay = GAPIntVectorToNmz(out[i], ELM_PLIST(M, i+1));
+        if (!okay)
+            return false;
+    }
+    return true;
+}
+
+Obj NmzVectorToGAP(const vector<long>& in)
+{
+    Obj M;
+    const size_t n = in.size();
+    M = NEW_PLIST(T_PLIST_CYC, n);
+    SET_LEN_PLIST(M, n);
+    for (size_t i=0; i < n; ++i)
+    {
+        SET_ELM_PLIST(M, i+1, ObjInt_Int(in[i]));
+        CHANGED_BAG( M );
+    }
+    return M;
+}
+
+Obj NmzMatrixToGAP(const vector< vector<long> >& in)
+{
+    Obj M;
+    const size_t n = in.size();
+    M = NEW_PLIST(T_PLIST_CYC, n);
+    SET_LEN_PLIST(M, n);
+    for (size_t i=0; i < n; ++i)
+    {
+        SET_ELM_PLIST(M, i+1, NmzVectorToGAP(in[i]));
+        CHANGED_BAG( M );
+    }
+    return M;
+}
+
+
+Obj NormalizTestCone(Obj self, Obj M)
+{
+/*    long M[5][3] = {{2, 0, 0},
+                    {1, 3, 1},
+                    {3, 3, 1},
+                    {0, 0, 2},
+                    {1,-3, 1}};
+
+    vector<long> row;
+    vector<vector<long> > Gens;
+    for (long i=0; i<5; ++i) {
+        row.assign(M[i], (M[i]) + 3);
+        Gens.push_back(row);
+    }
+*/
+
+    vector<vector<long> > Gens;
+    bool okay = GAPIntMatrixToNmz(Gens, M);
+    if (!okay)
+        return Fail;
+
+    map <InputType, vector< vector<long> > > input;
+    input[libnormaliz::Type::integral_closure] = Gens;
+    Cone<long> MyCone(input);
+
+//    MyCone.compute(libnormaliz::ConeProperty::HilbertBasis);
+    MyCone.compute(ConeProperties(libnormaliz::ConeProperty::HilbertBasis));
+    return NmzMatrixToGAP(MyCone.getHilbertBasis());
 }
 
 
@@ -37,7 +138,8 @@ typedef Obj (* GVarFunc)(/*arguments*/);
 
 // Table of functions to export
 static StructGVarFunc GVarFuncs [] = {
-    GVAR_FUNC_TABLE_ENTRY("normaliz.c", NormalizTest, 1, "xyz"),
+    GVAR_FUNC_TABLE_ENTRY("normaliz.cc", NormalizTest, 1, "xyz"),
+    GVAR_FUNC_TABLE_ENTRY("normaliz.cc", NormalizTestCone, 1, "Mat"),
 
 	{ 0 } /* Finish with an empty entry */
 
