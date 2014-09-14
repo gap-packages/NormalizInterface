93dce3f219157a26f981a88de0e2ff22e077fb69
Max Horn <max@quendi.de>
2014-09-14 21:00:24 +0200

    Fix typo

--- build-normaliz.sh
+++ build-normaliz.sh
@@ -3,14 +3,14 @@
 # This helper script compiles Normaliz for you.
 # Before using it the first time, clone the "gap" branch of
 # the Normaliz git repository:
-#    git clone -b gap https://github.com/csoeger/Normaliz Normaliz.git
+#    b
 #
 # After that you can run this script ("build-normaliz.sh").
 #
 # Once it completed successfully, you can then build NormalizInterface
 # like this:
 #
-#  ./configure --with-gaproot=GAPDR --with-normaliz=$PWD/Normaliz.git/DST
+#  ./configure --with-gaproot=GAPDIR --with-normaliz=$PWD/Normaliz.git/DST
 #  make
 #
 
