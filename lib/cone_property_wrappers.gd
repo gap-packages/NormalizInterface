62e3c632e0c6039057f4c63c2f2a1e78b957f093
Christof Soeger <csoeger@uos.de>
2015-10-02 11:18:26 +0200

    remove the gen_cone_property_wrappers.sh
    This closes #21

---
+++ lib/cone_property_wrappers.gd
@@ -0,0 +1,149 @@
+## This is an automatically generated file
+
+#! @Chapter Functions
+#! @Section Cone properties
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "AffineDim" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzAffineDim" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "ClassGroup" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzClassGroup" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "Deg1Elements" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzDeg1Elements" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "Dehomogenization" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzDehomogenization" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "ExcludedFaces" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzExcludedFaces" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "ExtremeRays" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzExtremeRays" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "Generators" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzGenerators" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "Grading" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzGrading" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "HilbertBasis" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzHilbertBasis" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "HilbertQuasiPolynomial" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzHilbertQuasiPolynomial" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "HilbertSeries" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzHilbertSeries" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "InclusionExclusionData" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzInclusionExclusionData" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "IsDeg1ExtremeRays" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzIsDeg1ExtremeRays" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "IsDeg1HilbertBasis" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzIsDeg1HilbertBasis" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "IsIntegrallyClosed" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzIsIntegrallyClosed" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "IsPointed" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzIsPointed" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "ModuleGenerators" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzModuleGenerators" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "ModuleGeneratorsOverOriginalMonoid" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzModuleGeneratorsOverOriginalMonoid" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "ModuleRank" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzModuleRank" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "Multiplicity" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzMultiplicity" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "OriginalMonoidGenerators" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzOriginalMonoidGenerators" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "RecessionRank" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzRecessionRank" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "IsReesPrimary" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzIsReesPrimary" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "ReesPrimaryMultiplicity" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzReesPrimaryMultiplicity" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "SupportHyperplanes" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzSupportHyperplanes" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "Triangulation" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzTriangulation" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "TriangulationDetSum" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzTriangulationDetSum" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "TriangulationSize" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzTriangulationSize" );
+
+#! @Description
+#!  This is an alias for <C>NmzConeProperty( cone, "VerticesOfPolyhedron" );</C>
+#! @Arguments cone
+DeclareGlobalFunction( "NmzVerticesOfPolyhedron" );
+
---
+++ lib/cone_property_wrappers.gi
@@ -0,0 +1,61 @@
+## This is an automatically generated file
+
+
+InstallGlobalFunction( NmzAffineDim, cone -> NmzConeProperty(cone, "AffineDim" ) );
+
+InstallGlobalFunction( NmzClassGroup, cone -> NmzConeProperty(cone, "ClassGroup" ) );
+
+InstallGlobalFunction( NmzDeg1Elements, cone -> NmzConeProperty(cone, "Deg1Elements" ) );
+
+InstallGlobalFunction( NmzDehomogenization, cone -> NmzConeProperty(cone, "Dehomogenization" ) );
+
+InstallGlobalFunction( NmzExcludedFaces, cone -> NmzConeProperty(cone, "ExcludedFaces" ) );
+
+InstallGlobalFunction( NmzExtremeRays, cone -> NmzConeProperty(cone, "ExtremeRays" ) );
+
+InstallGlobalFunction( NmzGenerators, cone -> NmzConeProperty(cone, "Generators" ) );
+
+InstallGlobalFunction( NmzGrading, cone -> NmzConeProperty(cone, "Grading" ) );
+
+InstallGlobalFunction( NmzHilbertBasis, cone -> NmzConeProperty(cone, "HilbertBasis" ) );
+
+InstallGlobalFunction( NmzHilbertQuasiPolynomial, cone -> NmzConeProperty(cone, "HilbertQuasiPolynomial" ) );
+
+InstallGlobalFunction( NmzHilbertSeries, cone -> NmzConeProperty(cone, "HilbertSeries" ) );
+
+InstallGlobalFunction( NmzInclusionExclusionData, cone -> NmzConeProperty(cone, "InclusionExclusionData" ) );
+
+InstallGlobalFunction( NmzIsDeg1ExtremeRays, cone -> NmzConeProperty(cone, "IsDeg1ExtremeRays" ) );
+
+InstallGlobalFunction( NmzIsDeg1HilbertBasis, cone -> NmzConeProperty(cone, "IsDeg1HilbertBasis" ) );
+
+InstallGlobalFunction( NmzIsIntegrallyClosed, cone -> NmzConeProperty(cone, "IsIntegrallyClosed" ) );
+
+InstallGlobalFunction( NmzIsPointed, cone -> NmzConeProperty(cone, "IsPointed" ) );
+
+InstallGlobalFunction( NmzModuleGenerators, cone -> NmzConeProperty(cone, "ModuleGenerators" ) );
+
+InstallGlobalFunction( NmzModuleGeneratorsOverOriginalMonoid, cone -> NmzConeProperty(cone, "ModuleGeneratorsOverOriginalMonoid" ) );
+
+InstallGlobalFunction( NmzModuleRank, cone -> NmzConeProperty(cone, "ModuleRank" ) );
+
+InstallGlobalFunction( NmzMultiplicity, cone -> NmzConeProperty(cone, "Multiplicity" ) );
+
+InstallGlobalFunction( NmzOriginalMonoidGenerators, cone -> NmzConeProperty(cone, "OriginalMonoidGenerators" ) );
+
+InstallGlobalFunction( NmzRecessionRank, cone -> NmzConeProperty(cone, "RecessionRank" ) );
+
+InstallGlobalFunction( NmzIsReesPrimary, cone -> NmzConeProperty(cone, "IsReesPrimary" ) );
+
+InstallGlobalFunction( NmzReesPrimaryMultiplicity, cone -> NmzConeProperty(cone, "ReesPrimaryMultiplicity" ) );
+
+InstallGlobalFunction( NmzSupportHyperplanes, cone -> NmzConeProperty(cone, "SupportHyperplanes" ) );
+
+InstallGlobalFunction( NmzTriangulation, cone -> NmzConeProperty(cone, "Triangulation" ) );
+
+InstallGlobalFunction( NmzTriangulationDetSum, cone -> NmzConeProperty(cone, "TriangulationDetSum" ) );
+
+InstallGlobalFunction( NmzTriangulationSize, cone -> NmzConeProperty(cone, "TriangulationSize" ) );
+
+InstallGlobalFunction( NmzVerticesOfPolyhedron, cone -> NmzConeProperty(cone, "VerticesOfPolyhedron" ) );
+
