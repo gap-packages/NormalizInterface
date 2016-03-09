7d003ca521d95ad5fa151d325251a574f7374b84
Max Horn <max@quendi.de>
2016-03-09 16:20:29 +0100

    remove reference to libsing.h

--- src/normaliz.h
+++ src/normaliz.h
@@ -22,7 +22,7 @@
 
 // Include gmp.h *before* switching to C mode, because GMP detects when compiled from C++
 // and then does some things differently, which would cause an error if
-// called from within extern "C". But libsing.h (indirectly) includes gmp.h ...
+// called from within extern "C".
 #include <gmp.h>
 
 extern "C" {
