05546345232b705f39baaf2b6dfe80beb0bb78a2
Max Horn <max@quendi.de>
2017-12-07 12:46:21 +0100

    Use C99 init for StructInitInfo

--- src/normaliz.cc
+++ src/normaliz.cc
@@ -1008,21 +1008,13 @@ static Int InitLibrary( StructInitInfo *module )
 */
 static StructInitInfo module = {
 #ifdef NORMALIZSTATIC
- /* type        = */ MODULE_STATIC,
+    .type = MODULE_STATIC,
 #else
- /* type        = */ MODULE_DYNAMIC,
+    .type = MODULE_DYNAMIC,
 #endif
- /* name        = */ "Normaliz",
- /* revision_c  = */ 0,
- /* revision_h  = */ 0,
- /* version     = */ 0,
- /* crc         = */ 0,
- /* initKernel  = */ InitKernel,
- /* initLibrary = */ InitLibrary,
- /* checkInit   = */ 0,
- /* preSave     = */ 0,
- /* postSave    = */ 0,
- /* postRestore = */ 0
+    .name = "Normaliz",
+    .initKernel = InitKernel,
+    .initLibrary = InitLibrary,
 };
 
 #ifndef NORMALIZSTATIC
