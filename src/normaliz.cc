ba2aece8b34c900bda8a512aca42731f024467aa
Chris Jefferson <caj21@st-andrews.ac.uk>
2022-02-24 12:05:44 +0000

    Remove unnessary casts
    On Cygwin, mp_limb_signed_t is long long, and there if no overload
    for this case. However, there is no need to cast.

--- src/normaliz.cc
+++ src/normaliz.cc
@@ -420,7 +420,7 @@ bool GAPToNmz(long & out, Obj x)
 bool GAPToNmz(mpz_class & out, Obj x)
 {
     if (IS_INTOBJ(x)) {
-        out = (mp_limb_signed_t)INT_INTOBJ(x);
+        out = INT_INTOBJ(x);
         return true;
     }
     else if (TNUM_OBJ(x) == T_INTPOS || TNUM_OBJ(x) == T_INTNEG) {
@@ -437,7 +437,7 @@ bool GAPToNmz(mpz_class & out, Obj x)
 bool GAPToNmz(mpq_class & out, Obj x)
 {
     if (IS_INTOBJ(x)) {
-        out = (mp_limb_signed_t)INT_INTOBJ(x);
+        out = INT_INTOBJ(x);
         return true;
     }
     else if (TNUM_OBJ(x) == T_INTPOS || TNUM_OBJ(x) == T_INTNEG) {
